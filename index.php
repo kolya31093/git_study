98a7bccfda5e5f7732fee67f8d784132b7194921
Kolya Keb <kolya31093@ro.ru>
2017-09-16 22:16:13 +0300

    first

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kolya
+ * Date: 14.07.2017
+ * Time: 19:12
+ */
+
+	// запуск сессии
+	session_start();
+	// подключение библиотек
+	require "a_db.php";
+    require "a_lib.inc.php";
+
+?>
+<html>
+<head>
+    <title>Каталог товаров</title>
+
+</head>
+
+<body>
+    <h1>Hello world!!!</h1>
+
+</body>
+</html>
